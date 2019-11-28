374dc3e671e0d7738e05693dacfa5bc4e7f66471
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-11-28 15:07:44 +0000

    Transfers smartgame interface into react app

--- client/src/App/pages/SmartGame.js
+++ client/src/App/pages/SmartGame.js
@@ -1,23 +1,79 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import $ from "jquery";
+import $ from 'jquery';
+import Letter from '../../model/letter'
 
 class SmartGame extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { smartGame: { playerLetters: ['A', 'B', 'C', 'D', 'E'],
+      skillPoints: 20,
+      smartPoints: 0,
+      possibleWords:[],
+      validWords: []
+    }};
+  }
 
   componentDidMount() {
+    const game = this.state.smartGame
+    const letterGetReq = game.playerLetters.join('').toLowerCase()
+    const letters = new Letter()
+    $('#validwordslist').html('')
 
-      const smartGame = {
-        this.playerLetters = ['A', 'B', 'C', 'D', 'E']
-        this.skillPoints = skillPoints
-        this.smartPoints = 0
-        this.possibleWords = []
-        this.validWords = []
-      }
-    }
+    $.get(`https://jsonp.afeld.me/?url=http://anagramica.com/all/:${letterGetReq}`, function(data) {
+        game.possibleWords = data.all.filter((w) => { if (w.length > 2) { return true } }).map((w) => {
+          return w.toUpperCase()
+        })
+    });
+
+    generateLetterButtons ()
+    let wordInput = ''
+    $('#typearea').text(wordInput)
 
     $('#clearbutton').click(() => {
-      console.log('clicked')
+      clearTextInput()
     })
+
+    $('[class*="letterbutton-on"]').click((event) => {
+      console.log(event.currentTarget.className)
+      if (event.currentTarget.className === "letterbutton-off") { return }
+      wordInput += event.currentTarget.innerHTML
+      event.currentTarget.className = "letterbutton-off"
+      $('#typearea').text(wordInput)
+      if (!game.validWords.includes(wordInput) && game.possibleWords.includes(wordInput)) {
+        game.validWords.push(wordInput)
+        savePoints()
+        clearTextInput()
+      }
+      $('#validwordslist').html(game.validWords.join(' - '))
+    })
+
+    function generateLetterButtons () {
+      const buttonHTML = game.playerLetters.map((letter) => {
+        const score = letters.getScore(letter)
+        return `<button class="letterbutton-on${score}" value="${score}">${letter}</button>`
+      })
+      $('#letterkeys').html(buttonHTML.join('\n'))
+    }
+
+    function clearTextInput () {
+      wordInput = ''
+      $('#typearea').text(wordInput)
+
+      $('.letterbutton-off').each((_index, button) => {
+        $(button).attr('class', `letterbutton-on${button.value}`)
+      })
+    }
+
+    function savePoints () {
+      wordInput.split('').forEach((char) => {
+        game.smartPoints += letters.getScore(char)
+      })
+      $('#score').text('Current Score: ' + game.smartPoints)
+    }
+
+    function gameOver() {
+    }
   }
 
   render() {
