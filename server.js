94135fd5ec4aab7d82723152462fa8bab5fa3f9f
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-12-02 16:04:32 +0000

    Amends server code.

--- server.js
+++ server.js
@@ -1,45 +1,38 @@
 const express = require('express');
-const app = express();
-const path = require('path');
-const port = process.env.PORT || 5000;
-
-
-
-
 const bodyParser= require('body-parser')
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.set('view engine', 'ejs')
+const path = require('path');
 
-
-if(process.env.NODE_ENV != 'production') {
-  app.listen(port, (req, res) => {  console.log( `server listening on port: ${port}`);})
-}
-
+const app = express();
+const port = process.env.PORT || 5000;
 const MongoClient = require('mongodb').MongoClient
 
-var db
-var list = []
+let db
 
 MongoClient.connect('mongodb+srv://jess:JL731996@cluster0-cfgis.mongodb.net/test?retryWrites=true&w=majority', (err, database) => {
-    if (err) return console.log(err)
-    db = database.db('wordball')
-  })
+  if (err) return console.log(err)
+  db = database.db('wordball')
+})
 
-  app.get('/api/getLeaderboard', (req,res) => {
-    console.log('reached')
-    list = []
-    db.collection('scores').find()
-    .toArray(function(err, results) {
-      results.forEach((item) => {
-        list.push(`${item.name} ---- skill: ${item.skillscore} - smart: ${item.smartscore}`)
-      })
-      res.json(list);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/api/getLeaderboard', (req,res) => {
+  console.log('reached')
+  let list = []
+  db.collection('scores').find()
+  .toArray(function(err, results) {
+    results.forEach((item) => {
+      list.push(`${item.name} ---- skill: ${item.skillscore} - smart: ${item.smartscore}`)
     })
+    res.json(list);
   })
+})
 
-  if(process.env.NODE_ENV === 'production') {
-    app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/build/index.html'));})
-  } else {
-    app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
-  }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
