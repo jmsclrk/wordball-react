c8dbf1182c16ac9abdb95f74cc4ad549bbd259fe
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-12-02 13:05:43 +0000

    Updates express path and package.json for deployment.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser= require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.set('view engine', 'ejs')
 
 
