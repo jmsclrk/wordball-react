de093c8b2631cd2190cee397701cb147c818fa57
JDtheGeek <john.dennehy@gmail.com>
2019-12-06 15:28:13 +0000

    adds additional rules

--- client/src/App/pages/Rules.js
+++ client/src/App/pages/Rules.js
@@ -37,9 +37,23 @@ export default function Rules() {
           <br />
           <p>Click to fire the balls! </p>
           <br />
-          <p>Fire down to save up to 8 letters for the word game, or generate extra points by firing unwanted letters into the bonus holes above.</p>
+          <p>Wordball Extreme is two games in one: A game of skill, and a game of mental <span>
+            <a href="https://dictionary.cambridge.org/dictionary/english/acuity">acuity</a>
+          </span>.</p>
           <br />
-          <p>Be careful not to choose the wrong letters!</p>
+          <p>The aim of the game is to score points using the random balls. Each ball has a letter, and each letter has a value linked to the frequency that letter is found in words.</p>
+          <br />
+          <p>The aim is to select the best 8 letters from the random selection, and place them in the rectangular 'word-hole' at the bottom of the play area.  If you don't like a letter, you can instead discard them firing them at the 'bonus-holes' at the top of the screen, each with a difficulty modifier.</p>
+          <br />
+          <p>Balls are fired upon clicking or tapping the screen. The ball is aimed at the point you clicked/tapped.</p>
+          <br />
+          <p>You will move to the next stage when the timer runs out. If you don't save any balls, your game will be over!</p>
+          <br />
+          <p>On the next screen you will tasked with creating as many words as possible from the balls you saved, so hopefully you chose well!</p>
+          <br />
+          <p>Points are calculated based on the value of the letters, and the length of the words. Be careful not to choose the wrong letters!</p>
+          <br />
+          <p>Good luck!</p>
         </div>
 
       </div>
